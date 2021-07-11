8bc37887c3beffbb1230caabb7344e9472effd22
FRY <fry444@gmail.com>
2021-07-11 12:34:38 -0300

    [DONE] Pantalla 1 mejorada

--- Assets/tiles1.tsx
+++ Assets/tiles1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="tiles2" tilewidth="32" tileheight="32" tilecount="270" columns="18">
- <image source="tiles2.png" width="586" height="480"/>
+<tileset version="1.5" tiledversion="1.7.0" name="tiles1" tilewidth="32" tileheight="32" tilecount="405" columns="27">
+ <image source="tiles1.png" width="886" height="480"/>
 </tileset>
